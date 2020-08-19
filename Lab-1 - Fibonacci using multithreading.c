0d2b36f1d7cc8342d9d7f01ccc429279f08fd453
Mourya Thodupunuri <58902130+thor-tm2011@users.noreply.github.com>
2020-08-19 16:19:59 +0530

    Update Lab-1 - Fibonacci using multithreading.c

--- Lab-1 - Fibonacci using multithreading.c
+++ Lab-1 - Fibonacci using multithreading.c
@@ -37,23 +37,3 @@ int main(int argc, char *argv[])
     printf("\n");
     return 0;
 }
-
-
-
-
-#include <stdio.h> 
-#include <sys/types.h> 
-#include <unistd.h> 
-void forkexample() 
-{ 
-    if (fork() == 0) 
-        printf("Hello from Child!\n"); 
- 
-    else
-        printf("Hello from Parent!\n"); 
-} 
-int main() 
-{ 
-    forkexample(); 
-    return 0; 
-}
